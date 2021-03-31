6f573d2375ea28f580fade5fc2af09557120aadf
Stephen-2001 <peers0212@gmail.com>
2021-03-30 23:22:36 -0700

    add main.cpp

--- main.cpp
+++ main.cpp
@@ -3,61 +3,74 @@
 
 uLCD_4DGL uLCD(D1, D0, D2); // serial tx, serial rx, reset pin;
 
-DigitalIn upbutton(D7);
-DigitalIn downbutton(D8);
-DigitalIn confirmbutton(D9);
+DigitalIn upbutton(D3);
+DigitalIn downbutton(D9);
+DigitalIn confirmbutton(D5);
 
+AnalogOut aout(PA_4);
+AnalogIn ain(D11);
+
+double ADCdata[500];
 
 int main(){
 	uLCD.text_width(1);
 	uLCD.text_height(1);
 	uLCD.color(RED);
 
-    int display_frequency = 100;
-	int frequency = 100;
-	bool uplimit = false;
-	bool downlimit = false;
-	bool confirmlimit = false;
+    int display_frequency = 30;
+	int frequency = 30;
+	double piece = 0.0;
+	double T = 1000.0 / frequency;
+	int index = 0;
+	int sample = 25;
+	int count=0;
 
 	while(1){
-		// set up the UP button
-		// after push, display_frequency +10
 		if(upbutton){
-			if(!uplimit){
-				display_frequency += 10;
-				uplimit = true;
-				ThisThread::sleep_for(1ms);
-			}
-		} else {
-			uplimit = false;
+			display_frequency += 10;
+			uLCD.locate(1, 2);
+			uLCD.printf("%4d %4d\n", display_frequency, frequency);
 		}
-		// set up the DOWN button
-		// after push, display_frequency -10
 		if(downbutton){
-			if(!downlimit){
-				display_frequency -= 10;
-				downlimit = true;
-				ThisThread::sleep_for(1ms);
-			}
-		} else {
-			downlimit = false;
+			display_frequency -= 10;
+			uLCD.locate(1, 2);
+			uLCD.printf("%4d %4d\n", display_frequency, frequency);
 		}
-		// set up the CONFIRM button
-		// after push, frequency = display_frequency
+		// the GUI bar display the scale of frequency
+		// uLCD.line(0, 100, display_frequency/10, 100, WHITE);
+		// uLCD.line(display_frequency/10, 100, 1000, 100, RED);
 		if (confirmbutton){
-			if(!confirmlimit){
-				frequency = display_frequency;
-				confirmlimit = true;
-				ThisThread::sleep_for(1ms);
+			frequency = display_frequency;
+			T = 1000.0 / frequency;
+			uLCD.locate(1, 2);
+			uLCD.printf("%4d %4d\n", display_frequency, frequency);
+			sample = 100 * frequency / 500;
+			for(int i=0; i<500; i++){
+				printf("%.3f\r\n", ADCdata[i]);
 			}
-		} else {
-			confirmlimit = false;
 		}
+		// display the next frequency we choose and current frequency
+		
+		if(piece >= T){
+			piece = 0;
+		}
+		else if(piece <= 0.5*T){
+			aout = piece / (0.55*T);
+		} 
+		else {
+			aout = (T-piece) / (0.55*T);
+		}
+		piece += (T/100.0);
 
-		uLCD.locate(1, 2);
-		uLCD.printf("%4d %4d\n", display_frequency, frequency);
-		uLCD.line(0, 100, display_frequency/10, 100, WHITE);
-		uLCD.line(display_frequency/10, 100, 1000, 100, RED);
+		if(index >= 500){
+			count = 0;
+			index = 0;
+		}
+		else if(index<500 && (count % sample == 0)){
+			ADCdata[index] = ain;
+			index++;
+		}
+		count++;
+		wait_us(T*10-30);
 	}
-	
 }
